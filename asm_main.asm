67cdaa025c2050e1248fa300ab46994e992baf9c
matthew sprigg <matthewsprigg@cis.vvc.edu>
2018-09-07 19:15:52 -0700

    Added section deliniation.

--- asm_main.asm
+++ asm_main.asm
@@ -1,28 +1,55 @@
 ;
 ; file: asm_main.asm
+; Author
+; Description: 
+;
+;
 
 %include "asm_io.inc"
-;
+
+
+
+;--------------------------------------------------------
 ; initialized data is put in the .data segment
 ;
 segment .data
 
 
+;--------------------------------------------------------
 ; uninitialized data is put in the .bss segment
 ;
 segment .bss
 
-;
+
+;--------------------------------------------------------
 ; code is put in the .text segment
 ;
 segment .text
         global  asm_main
+
+
+
+;--------------------------------------------------------
+; MAIN FUNCTION						;
+;--------------------------------------------------------
+
 asm_main:
+
+;--------------------------------------------------------
+; PROLOGUE						;
+;--------------------------------------------------------
         enter   0,0               ; setup routine
         pusha
-; next print out result message as series of steps
 
+;--------------------------------------------------------
+; EPILOGUE						;
+;--------------------------------------------------------
         popa
         mov     eax, 0            ; return back to C
         leave
         ret
+
+
+;--------------------------------------------------------
+; FUNCTIONS / SUBPROGRAMS			 	;
+;--------------------------------------------------------
